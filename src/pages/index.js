2ffb146d053935eb0cb291840612c02bf1a3582d
weiiew <weiwei.li@honeybeehub.ca>
2020-02-04 08:18:44 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,27 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import axios from "axios"
+import io from "socket.io-client"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div>
+    <button
+      onClick={() =>
+        axios
+          .get("http://localhost:3000")
+          .then(result => console.log(result))
+          .catch(error => console.log("ERROR: " + error))
+      }
+    >
+      TEST HTTP
+    </button>
+    <button
+      onClick={() => {
+        const socket = io("http://localhost:3000/chat")
+      }}
+    >
+      TEST SOCKET
+    </button>
+  </div>
 )
 
 export default IndexPage
